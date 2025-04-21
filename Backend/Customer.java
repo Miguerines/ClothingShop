4d9b35616846434e56c6767c1c52ae5f39d1b0a0
Miguel Contreras <migerinesp@gmail.com>
2025-04-20 22:07:41 -0500

    Fisrt commit with the basic classes

---
+++ Backend/Customer.java
@@ -0,0 +1,60 @@
+import java.util.List;
+
+/**
+ * Represents a customer in the clothing store system.
+ * Extends Person with shopping cart functionality.
+ */
+public class Customer extends Person {
+    private ShoppingCart cart;
+
+    /**
+     * Constructor for Customer
+     * @param firstName Customer's first name
+     * @param lastName Customer's last name
+     * @param email Customer's email
+     * @param password Customer's password
+     */
+    public Customer(String firstName, String lastName, String email, String password) {
+        super(firstName, lastName, email, password);
+        this.cart = new ShoppingCart();
+    }
+
+    /**
+     * Displays all available clothing products
+     * @param inventory List of all available products
+     */
+    public void viewProducts(List<Clothing> inventory) {
+        if (!isLoggedIn()) {
+            System.out.println("Please login to view products");
+            return;
+        }
+        
+        System.out.println("Available Products:");
+        for (Clothing product : inventory) {
+            System.out.println(product.toString());
+        }
+    }
+
+    /**
+     * Adds a product to the customer's shopping cart
+     * @param product Clothing item to add
+     * @param quantity Quantity to add
+     * @return true if operation was successful, false otherwise
+     */
+    public boolean addToCart(Clothing product, int quantity) {
+        if (!isLoggedIn()) {
+            System.out.println("Please login to add items to your cart");
+            return false;
+        }
+        
+        return cart.addItem(product, quantity);
+    }
+
+    /**
+     * Gets the customer's shopping cart
+     * @return The ShoppingCart object
+     */
+    public ShoppingCart getCart() {
+        return cart;
+    }
+}
---
+++ Backend/Person.java
@@ -0,0 +1,86 @@
+/**
+ * Base class representing a person in the system.
+ * Contains common attributes and methods for authentication.
+ */
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private boolean isLoggedIn;
+
+    /**
+     * Constructor for Person
+     * @param firstName Person's first name
+     * @param lastName Person's last name
+     * @param email Email address (used as username)
+     * @param password Login password
+     */
+    public Person(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.isLoggedIn = false;
+    }
+
+    /**
+     * Authenticates the user and starts a session
+     * @param email Email to authenticate
+     * @param password Password to authenticate
+     * @return true if authentication was successful, false otherwise
+     */
+    public boolean login(String email, String password) {
+        if (this.email.equals(email) && this.password.equals(password)) {
+            this.isLoggedIn = true;
+            System.out.println("Login successful for " + this.firstName);
+            return true;
+        }
+        System.out.println("Invalid email or password");
+        return false;
+    }
+
+    /**
+     * Ends the current user session
+     */
+    public void logout() {
+        this.isLoggedIn = false;
+        System.out.println("Logout successful for " + this.firstName);
+    }
+
+    // Getters and Setters
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Checks if the user is currently logged in
+     * @return true if user is logged in, false otherwise
+     */
+    public boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+    // Note: No setter for password for security reasons
+}
