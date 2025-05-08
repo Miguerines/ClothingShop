d85ed60e699ecae4134f9b7da160fbc4348d7d3c
andreslmmm <al9834018@gmail.com>
2025-05-07 19:52:02 -0500

    Add files via upload

---
+++ tiendaderopa/src/main/java/com/udistrital/tiendaderopa/Shirt.java
@@ -0,0 +1,10 @@
+package com.udistrital.tiendaderopa;
+
+
+import com.udistrital.tiendaderopa.Clothing;
+
+public class Shirt extends Clothing {
+    public Shirt(String id, String name, String description, double price, int stockQuantity, String size, String color) {
+        super(id, name, description, price, stockQuantity, size, color);
+    }
+}
