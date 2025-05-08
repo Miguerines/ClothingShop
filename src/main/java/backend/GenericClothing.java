adb209a88a2e86faefd6305f125e63ace4b9701a
Miguel Contreras <migerinesp@gmail.com>
2025-05-07 20:54:10 -0500

    All the frontend

---
+++ src/main/java/backend/GenericClothing.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package backend;
+
+/**
+ * Clase genérica para representar un artículo de ropa.
+ */
+public class GenericClothing extends Clothing {
+    public GenericClothing(String id, String name, String description, double price, int stockQuantity, String size, String color) {
+        super(id, name, description, price, stockQuantity, size, color);
+    }
+}
